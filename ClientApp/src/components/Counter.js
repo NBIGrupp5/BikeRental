080437fa8c204046c97f94e74d375223a6d8a59b
ItsSteph <steph.tronier.vip@gmail.com>
2021-05-05 14:47:21 +0200

    removed link ref

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -17,9 +17,7 @@ export class Counter extends Component {
 
   render() {
       return (
-      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      
 
       <div>
         <h1>Guzal är awesome</h1>
