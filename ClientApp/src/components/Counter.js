e91d71c85d35f7e19907bca7173fcdb220242e04
ItsSteph <steph.tronier.vip@gmail.com>
2021-05-05 14:42:53 +0200

    changed name from Stephanie to Guzal

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -13,12 +13,16 @@ export class Counter extends Component {
     this.setState({
       currentCount: this.state.currentCount + 1
     });
-  }
+    }
 
   render() {
-    return (
+      return (
+      <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
       <div>
-        <h1>Stephanie är awesome</h1>
+        <h1>Guzal är awesome</h1>
 
         <p>This is a simple example of a React component.</p>
 
