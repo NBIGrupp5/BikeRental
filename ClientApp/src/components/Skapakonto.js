ad3eb55752d1aad069f8aa7dd1aa8f3f2ef72a4b
NinmarAcar <73597589+NinmarAcar@users.noreply.github.com>
2021-06-04 10:23:20 +0200

    Heeej

---
+++ ClientApp/src/components/Skapakonto.js
@@ -0,0 +1,42 @@
+﻿import React, { Component } from 'react';
+import './Skapakonto.css'
+
+export class Skapakonto extends Component {
+    static displayName = Skapakonto.name;
+
+    render() {
+        return (
+            <html>
+                <body>
+                    <div id="id01" class="modal" />
+                    <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+                    <form class="modal-content" action="/action_page.php" />
+                    <div class="container" />
+                    <h1>Skapa Konto</h1>
+                    <hr />
+                    <label for="email"><b>Email</b></label><br />
+                    <input type="text" placeholder="Skriv in Email" name="email" required />
+                    <br />
+                    <label for="psw"><b>Lösenord</b></label><br />
+                    <input type="password" placeholder="Skriv in lösenord" name="psw" required />
+                    <br />
+                    <label for="psw-repeat"><b>Upprepa lösenord</b></label><br />
+                    <input type="password" placeholder="Skriv in lösenord" name="psw-repeat" required />
+                    <br />
+                    <label />
+                    <input type="checkbox" checked="checked" name="remember" style={{ bottom: "15px" }} /> Komma ihåg
+                    <body>
+                        <label />
+
+                        <p>Genom att skapa ett konto så accepterar du våra <a href="#" style={{ color: "dodgerblue" }}>Vilkor & Integritet</a>.</p>
+
+                        <div class="clearfix">
+                            <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Avbryt</button>
+                            <button type="submit" class="signupbtn">Skapa Konto</button>
+                        </div>
+                    </body>
+                </body>
+            </html>
+        )
+    }
+}
