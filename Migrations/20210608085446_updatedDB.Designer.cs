9eb5d22a67267b09cb7b95985234aa620f45894d
Guzal Bulatova <guzalbulatova@gmail.com>
2021-06-08 14:23:26 +0200

    added cypress and "link to booking form" test

--- Migrations/20210608085446_updatedDB.Designer.cs
+++ Migrations/20210608085446_updatedDB.Designer.cs
@@ -4,14 +4,16 @@ using BikeRental;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BikeRental.Migrations
 {
     [DbContext(typeof(BikeRentalContext))]
-    partial class BikeRentalContextModelSnapshot : ModelSnapshot
+    [Migration("20210608085446_updatedDB")]
+    partial class updatedDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
